05afd910e24a905dab0d547f4f6b422244e8e467
Ashish Sabharwal <AshishS@allenai.org>
2015-11-05 19:36:06 -0800

    change nThreads from 4 to 8; add author

--- arith-cplex/scripts/runall.sh
+++ arith-cplex/scripts/runall.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+## Author: Ashish Sabharwal, AI2, November 2015
+
+## A script to run arithCplex executable on every math problem ILP input file in the
+## specified folder. The output for each math problem is saved in a text file with
+## the same name as the input file extended with a suffix. The script by default
+## expects to find ../arithCplex and ../weights.conf, and launches CPLEX in
+## multi-threaded mode.
+
 if [ $# -ne 1 ]; then
   echo "USAGE: ./runall.sh datafolder"
   exit 1
@@ -13,7 +21,7 @@ weightsFile=../weights.conf
 insuffix=".txt"                        
 outSuffix=".out"
 nSolns=1000
-nThreads=4
+nThreads=8
 timelimit=30    # seconds
 
 inputFiles=$datafolder/*$insuffix
